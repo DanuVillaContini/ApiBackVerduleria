d6ccb571f74a42d1d19b1a4148599faf242d2e3d
daniela <daniela.lucia.villa@hotmail.com>
2023-11-13 00:39:28 -0300

    corregido config

--- src/config.js
+++ src/config.js
@@ -6,3 +6,7 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || '9639asdQW'
 export const DB_NAME = process.env.DB_NAME || 'sistmetododb'
 export const DB_PORT = process.env.DB_PORT || 3306
 
+export const NODE_ENV = 'production';
+
+
+
