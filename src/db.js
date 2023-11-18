dcf76ec107bb38116ec5f942918fb980322e3603
daniela <daniela.lucia.villa@hotmail.com>
2023-11-18 13:23:01 -0300

    se modifico variables de entorno y asignaciones mal puesta como el nombre de la db que no coincidia con otros lados

--- src/db.js
+++ src/db.js
@@ -1,16 +1,18 @@
 import { createPool } from "mysql2/promise";
-import { DB_HOST,
-    DB_USER,
+import { 
+    DB_DATABASE,
+    DB_HOST,
     DB_PASSWORD,
-    DB_NAME,
-    DB_PORT } from "./config.js"
+    DB_PORT,
+    DB_USER
+} from "./config.js"
 
 export const connectMysql = createPool({
+    host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
-    host: DB_HOST,
     port: DB_PORT,
-    database: DB_NAME,
+    database: DB_DATABASE,
 })
 
 // connectMysql.query
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-import express from "express";
-import { PORT } from "./config.js";
-import EmpleadosRoutes from "./routes/empleados.routes.js"
-import IndexRoutes from "./routes/index.routes.js"
-
-
-const app = express()
-
-app.use(express.json())
-// Middlewares
-
-
-
-//Routes
-app.use('/api',EmpleadosRoutes)
-app.use(IndexRoutes)
-
-//ejecutar las peticiones antes de que lleguen --> con Morgan
-// app.use(morgan('dev'));
+import app from "./app.js"
+import { PORT } from "./config.js"
 
 app.listen(PORT)
-console.log('server on port', PORT)
+console.log(`Server on port http://localhost:${PORT}`)
