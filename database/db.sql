e5fc303f1d6c4234da9660b1819197a050ab7d8c
daniela <daniela.lucia.villa@hotmail.com>
2023-11-18 12:13:45 -0300

    carpeta database movida

--- database/db.sql
+++ database/db.sql
@@ -14,13 +14,10 @@ CREATE TABLE IF NOT EXISTS empleados (
 );
 
 INSERT INTO empleados (name, apellido, telefono, correo) VALUES
-('Juan', 'Pérez', '123456789', 'juan.perez@email.com'),
 ('María', 'Gómez', '987654321', 'maria.gomez@email.com'),
 ('Carlos', 'Rodríguez', '555123456', 'carlos.rodriguez@email.com'),
 ('Laura', 'Fernández', '333555777', 'laura.fernandez@email.com'),
-('Pedro', 'López', '777888999', 'pedro.lopez@email.com'),
-('Ana', 'Martínez', '111222333', 'ana.martinez@email.com'),
-('Roberto', 'Sánchez', '444555666', 'roberto.sanchez@email.com');
+('Pedro', 'López', '777888999', 'pedro.lopez@email.com');
 
 
 -- Insertar un registro de prueba
