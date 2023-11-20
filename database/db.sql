ce30923a31c614052b2d568daaa796fca9da695f
daniela <daniela.lucia.villa@hotmail.com>
2023-11-20 19:22:56 -0300

    ventas lesto

--- database/db.sql
+++ database/db.sql
@@ -17,68 +17,79 @@ SELECT * FROM acreedores;
 
 
 -- Crear la tabla de empleados
-CREATE TABLE IF NOT EXISTS empleados (
-    id INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    apellido VARCHAR(60) NOT NULL,
-    telefono VARCHAR(15), 
-    correo VARCHAR(100) UNIQUE 
-);
-CREATE TABLE IF NOT EXISTS proveedores (
-    id INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name_empresa VARCHAR(60) NOT NULL,
-    name_encargado VARCHAR(80) NOT NULL,
-    telefono VARCHAR(15), 
-    direccion VARCHAR(100) UNIQUE 
-);
-CREATE TABLE IF NOT EXISTS productos (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
-    tipo VARCHAR(60) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS stock (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    ProductoId INT NOT NULL,
-    Cantidad INT NOT NULL,
-    FOREIGN KEY (ProductoId) REFERENCES productos(id)
-);
-CREATE TABLE IF NOT EXISTS clientes (
-    id INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name_apellido VARCHAR(80) NOT NULL,
-    telefono VARCHAR(15) NOT NULL, 
-    direccion VARCHAR(100) UNIQUE 
-);
-CREATE TABLE IF NOT EXISTS ventas (
-    num_venta INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    clientes_id INT(8) NOT NULL,
-    total DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (clientes_id) REFERENCES clientes(id)
-);
+-- CREATE TABLE IF NOT EXISTS empleados (
+--     id INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(60) NOT NULL,
+--     apellido VARCHAR(60) NOT NULL,
+--     telefono VARCHAR(15), 
+--     correo VARCHAR(100) UNIQUE 
+-- );
+-- CREATE TABLE IF NOT EXISTS proveedores (
+--     id INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     name_empresa VARCHAR(60) NOT NULL,
+--     name_encargado VARCHAR(80) NOT NULL,
+--     telefono VARCHAR(15), 
+--     direccion VARCHAR(100) UNIQUE 
+-- );
+-- CREATE TABLE IF NOT EXISTS productos (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(60) NOT NULL,
+--     tipo VARCHAR(60) NOT NULL
+-- );
+-- CREATE TABLE IF NOT EXISTS stock (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     ProductoId INT NOT NULL,
+--     Cantidad INT NOT NULL,
+--     FOREIGN KEY (ProductoId) REFERENCES productos(id)
+-- );
+-- CREATE TABLE IF NOT EXISTS clientes (
+--     id INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     name_apellido VARCHAR(80) NOT NULL,
+--     telefono VARCHAR(15) NOT NULL, 
+--     direccion VARCHAR(100) UNIQUE 
+-- );
+
+-- CREATE TABLE IF NOT EXISTS ventas (
+--     id INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     clientes_id INT(8) NOT NULL,
+--     total DECIMAL(10,2) NOT NULL,
+--     FOREIGN KEY (clientes_id) REFERENCES clientes(id)
+-- );
+
 CREATE TABLE IF NOT EXISTS pedidos (
-    num_pedido INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     clientes_id INT(8) NOT NULL,
     total DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (clientes_id) REFERENCES clientes(id)
 );
+
 CREATE TABLE IF NOT EXISTS acreedores (
-    num_acree INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     clientes_id INT(8) NOT NULL,
-    num_venta INT(8) NOT NULL,
+    ventas_id INT(8) NOT NULL,
     total_deudor DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (clientes_id) REFERENCES clientes(id),
-    FOREIGN KEY (num_venta) REFERENCES ventas(num_venta)
+    FOREIGN KEY (ventas_id) REFERENCES ventas(id)
 );
--- Insertar datos en la tabla clientes
-INSERT INTO clientes (name_apellido, telefono, direccion) VALUES
-('Juan Pérez', '123-456-7890', 'Calle A #123'),
-('María López', '987-654-3210', 'Avenida B #456'),
-('Pedro Gómez', '555-123-4567', 'Carrera C #789');
 
--- Insertar datos en la tabla ventas
-INSERT INTO ventas (clientes_id, total) VALUES
-(1, 150.00),
-(2, 200.50),
-(3, 100.75);
+
+
+
+
+
+
+
+-- -- Insertar datos en la tabla clientes
+-- INSERT INTO clientes (name_apellido, telefono, direccion) VALUES
+-- ('Juan Pérez', '123-456-7890', 'Calle A #123'),
+-- ('María López', '987-654-3210', 'Avenida B #456'),
+-- ('Pedro Gómez', '555-123-4567', 'Carrera C #789');
+
+-- -- Insertar datos en la tabla ventas
+-- INSERT INTO ventas (clientes_id, total) VALUES
+-- (1, 150.00),
+-- (2, 200.50),
+-- (3, 100.75);
 
 -- Insertar datos en la tabla pedidos
 INSERT INTO pedidos (clientes_id, total) VALUES
@@ -87,17 +98,10 @@ INSERT INTO pedidos (clientes_id, total) VALUES
 (1, 120.00);
 
 -- Insertar datos en la tabla acreedores
-INSERT INTO acreedores (clientes_id, num_venta, total_deudor) VALUES
+INSERT INTO acreedores (clientes_id, ventas_id, total_deudor) VALUES
 (3, 1, 30.00),
 (1, 2, 50.50),
 (2, 3, 25.75);
-
-
-
-
-
-
-
 
 
 
--- src/controllers/ventas.controller.js
+++ src/controllers/ventas.controller.js
@@ -1,22 +1,91 @@
-import { connectMysql } from "../db.js"
+import { connectMysql } from "../db.js";
 
-export const  getVentas = async (req, res) => {
+export const getVentas = async (req, res) => {
     try {
-        const [rows] = await connectMysql.query("SELECT * FROM ventas");
+        const [rows] = await connectMysql.query("SELECT ventas.*, clientes.name_apellido as venta_nameCliente FROM ventas JOIN clientes ON ventas.clientes_id = clientes.id");
         res.json(rows);
     } catch (error) {
-        return res.status(500).json({ message: "Ups, algo salio mal" });
+        return res.status(500).json({ message: "Ups, algo salió mal" });
     }
-}
-export const  getVentasId = async (req, res) => {
+};
 
-}
-export const  createVentas = async (req, res) => {
+export const getVentasId = async (req, res) => {
+    try {
+        const { id } = req.params;
 
-}
-export const  updateVentas = async (req, res) => {
+        const [rows, fields] = await connectMysql.query(`
+            SELECT ventas.*, clientes.name_apellido as venta_nameCliente FROM ventas 
+            JOIN clientes ON ventas.clientes_id = clientes.id
+            WHERE ventas.id = ?  -- Cambiado de num_venta a id
+        `, [id]);
 
-}
-export const  deleteVentas = async (req, res) => {
+        if (rows.length <= 0) {
+            return res.status(404).json({ message: "Venta no encontrada" });
+        }
 
-}
+        res.json(rows[0]);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Ups, algo salió mal" });
+    }
+};
+
+
+export const createVentas = async (req, res) => {
+    try {
+        const { clientes_id, total } = req.body;
+
+        const [result] = await connectMysql.query(
+            "INSERT INTO ventas (clientes_id, total) VALUES (?, ?)",
+            [clientes_id, total]
+        );
+
+        const insertedId = result.insertId;
+
+        const [newRow] = await connectMysql.query("SELECT * FROM ventas WHERE id = ?", [insertedId]);
+
+        res.status(201).json({ mensaje: "Venta creada correctamente", venta: newRow[0] });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Ups, algo salió mal al crear la venta" });
+    }
+};
+
+export const updateVentas = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { clientes_id, total } = req.body;
+
+        const [result] = await connectMysql.query(
+            "UPDATE ventas SET clientes_id = ?, total = ? WHERE id = ?",
+            [clientes_id, total, id]
+        );
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Venta no encontrado" });
+        }
+
+        const [updatedRow] = await connectMysql.query("SELECT * FROM ventas WHERE id = ?", [id]);
+
+        res.status(200).json({ mensaje: "Ventas actualizado correctamente", ventas: updatedRow[0] });
+    } catch (error) {
+        return res.status(500).json({ message: "Ups, algo salió mal al actualizar el Ventas" });
+    }
+};
+
+export const deleteVentas = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const [result] = await connectMysql.query("DELETE FROM ventas WHERE id = ?", [id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Venta no encontrada o ya fue eliminada" });
+        }
+
+        res.status(200).json({ message: "Venta eliminada correctamente" });
+    } catch (error) {
+        console.error("Error al eliminar la venta:", error);
+        return res.status(500).json({ message: "Ups, algo salió mal al eliminar la venta" });
+    }
+};
